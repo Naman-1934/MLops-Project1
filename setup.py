5cf6bb01fbb1285622776231dc1c7eb1050f9368
Naman <naman532002@gmail.com>
2025-09-24 15:58:20 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Naman Shah",
+    author_email="shahnaman755@gamail.com",
+    packages=find_packages()
+)
